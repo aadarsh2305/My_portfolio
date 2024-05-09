173f5d762942b1467974da94370d654f4d49f106
Aadarsh Gupta <Aadarsh.Gupta@Prolifics.com>
2024-05-09 10:48:20 +0530

    inserted some more components

--- my-folio/src/sub-component/Home.jsx
+++ my-folio/src/sub-component/Home.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import './Home.css'
+import Typewriter from "typewriter-effect";
 
 function Home() {
   return (
     <div className="middle-container">
       <h2>Hello, my name is</h2>
       <h1>Aadarsh Gupta.</h1>
-      <p>I love exploring new things!</p>
+      <h1 className='diff_h1'>I love <span>
+            <Typewriter
+                options={{
+                    strings: ['exploring tech!',
+                    'coding solutions!',
+                    'building software!',
+                    'solving problems!',
+                    'learning constantly!',
+                    'innovating!',],
+                    autoStart: true,
+                    loop: true,
+                }}
+            />
+            </span>  
+       </h1>
       <p>I'm a Associate software engineer currently working in a Prolifics company in Hyderabad.</p>
       <p>I enjoy learning new skills and implementing them in real life!</p>
-      <button className="get-in-touch-btn">Get In Touch</button>
+      <p className="p_button"><a className='get-in-touch-btn'>Get In Touch</a></p>
     </div>
   )
 }
